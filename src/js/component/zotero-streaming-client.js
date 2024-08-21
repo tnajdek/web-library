b5d2a0b2d34dcccbfc6f05610787287ffb40e212
Tom Najdek <tom@doppnet.com>
2024-08-21 12:49:20 +0200

    Make streaming client more resillient
    * Add reconnect schedule
    * Reconnect if subscription confirmation is not received

--- src/js/component/zotero-streaming-client.js
+++ src/js/component/zotero-streaming-client.js
@@ -4,6 +4,21 @@ import { useDispatch, useSelector } from 'react-redux';
 import { remoteLibraryUpdate } from '../actions';
 import { getLibraryKeyFromTopic } from '../utils';
 
+// same as intervals in Zotero.Streamer_Module
+// https://github.com/zotero/zotero/blob/182bfb9a38bf4046fa1eb33f8a46dddcf2bade16/chrome/content/zotero/xpcom/streamer.js#L262
+const intervals = [
+	2, 5, 10, 15, 30, // first minute
+	60, 60, 60, 60, // every minute for 4 minutes
+	120, 120, 120, 120, // every 2 minutes for 8 minutes
+	300, 300, // every 5 minutes for 10 minutes
+	600, // 10 minutes
+	1200, // 20 minutes
+	1800, 1800, // 30 minutes for 1 hour
+	3600, 3600, 3600, // every hour for 3 hours
+	14400, 14400, 14400, // every 4 hours for 12 hours
+	86400 // 1 day
+].map(i => i * 1000);
+
 // Streaming client configures WS connection once, based on config
 // that never changes. There is no need to recreate handle* or connect
 // methods at any point therefore exhaustive-deps rule can be disabled
@@ -16,24 +31,46 @@ const ZoteroStreamingClient = () => {
 	const externalLibraries = useSelector(state => state.config.libraries).filter(l => l.isExternal);
 	const ws = useRef(null);
 	const isTearDown = useRef(false);
+	const retryMs = useRef(null);
+	const retryTimeout = useRef(null);
+	const retryIntervalIndex = useRef(0);
+
+	const closeUnconfirmedSubscription = useCallback(() => {
+		console.warn(`Failed to confirm subscription creation within ${retryMs.current / 1000} seconds. Closing connection.`);
+		ws.current.close();
+	}, []);
+
+	const createSubscriptions = useCallback(() => {
+		const subscriptions = [];
+		if (apiKey) {
+			subscriptions.push({ apiKey });
+		}
+
+		if (externalLibraries.length > 0) {
+			subscriptions.push({
+				topics: externalLibraries.map(l => `/${l.isGroupLibrary ? 'groups' : 'users'}/${l.id}`)
+			});
+		}
+		ws.current.send(JSON.stringify({ 'action': 'createSubscriptions', subscriptions }));
+
+		if (retryMs.current) {
+			retryTimeout.current = setTimeout(closeUnconfirmedSubscription, retryMs.current);
+		}
+	}, [apiKey, externalLibraries]);
 
 	const handleMessage = useCallback(ev => {
 		try {
 			const message = JSON.parse(ev.data);
 			if(message.event === 'connected') {
-				const subscriptions = [];
-				if(apiKey) {
-					subscriptions.push({ apiKey});
+				if(message.retry) {
+					retryMs.current = message.retry;
 				}
-
-				if(externalLibraries.length > 0) {
-					subscriptions.push({
-						topics: externalLibraries.map(l => `/${l.isGroupLibrary ? 'groups' : 'users'}/${l.id}`)
-					});
-				}
-				ws.current.send(JSON.stringify({'action': 'createSubscriptions', subscriptions }));
+				createSubscriptions();
 			} else if(message.event === 'subscriptionsCreated') {
-				//
+				if(retryTimeout.current) {
+					clearTimeout(retryTimeout.current);
+					retryIntervalIndex.current = 0;
+				}
 			} else if(message.event === 'topicUpdated') {
 				const libraryKey = getLibraryKeyFromTopic(message.topic);
 				if(libraryKey) {
@@ -48,7 +85,11 @@ const ZoteroStreamingClient = () => {
 
 	const handleClose = useCallback(() => {
 		if(!isTearDown.current) {
-			connect();
+			clearTimeout(retryTimeout.current);
+			retryMs.current = null;
+			console.warn(`Connection to the streaming API was closed. Retrying in ${intervals[retryIntervalIndex.current] / 1000} seconds`);
+			setTimeout(connect, intervals[retryIntervalIndex.current]);
+			retryIntervalIndex.current = Math.min(retryIntervalIndex.current + 1, intervals.length - 1);
 		}
 	}, []);
 
@@ -62,7 +103,9 @@ const ZoteroStreamingClient = () => {
 			ws.current.onerror = handleError;
 			ws.current.onclose = handleClose;
 		} catch(e) {
-			console.error('Failed to estabilish connection to the streaming API.');
+			console.error(`Failed to estabilish connection to the streaming API. Retrying in ${intervals[retryIntervalIndex.current] / 1000} seconds`);
+			setTimeout(connect, intervals[retryIntervalIndex.current]);
+			retryIntervalIndex.current = Math.min(retryIntervalIndex.current + 1, intervals.length - 1);
 		}
 	}, [handleClose, handleError, handleMessage, streamingApiUrl]);
 
