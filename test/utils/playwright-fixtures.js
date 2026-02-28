854ce96f3ef76c03669f150c1f4acd8f4f858152
Tom Najdek <tom@doppnet.com>
2026-03-01 00:17:27 +0100

    Switch to dynamic port allocation in Playwright fixtures
    This enables running individual playwright test files in parallel

--- test/utils/playwright-fixtures.js
+++ test/utils/playwright-fixtures.js
@@ -1,9 +1,20 @@
 import { test as baseTest } from '@playwright/test';
-import { getPort } from './fixed-state-server.js';
+import { createServer } from 'node:net';
+
+async function findFreePort() {
+	return new Promise((resolve, reject) => {
+		const server = createServer();
+		server.listen(0, () => {
+			const port = server.address().port;
+			server.close(() => resolve(port));
+		});
+		server.on('error', reject);
+	});
+}
 
 export const test = baseTest.extend({
-	serverPort: [async ({ }, use, workerInfo) => {
-		const port = getPort(workerInfo.workerIndex + 1);
+	serverPort: [async ({ }, use) => {
+		const port = await findFreePort();
 		await use(port);
 	}, { scope: 'worker' }],
 });
